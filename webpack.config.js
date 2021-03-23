70d90ebe48452d3a835fe1a81410e6c5da716187
Charles McCarthy <charles@studybundles.com>
2021-03-23 20:54:09 +0000

    Updated Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
-// const path = require('path')
-//
-// module.exports = {
-//     entry: './src/index.js',
-//     target: 'node', // support native modules
-//     output: {
-//         filename: 'main.js',
-//         path: path.resolve(__dirname, 'dist')
-//     }
-// }
+const root = require('app-root-path').path;
+
+module.exports = {
+    entry: `${root}/src/bin/www.js`,
+    target: 'node',
+    externals: [
+        /^[a-z\-0-9]+$/ // Ignore node_modules folder
+    ],
+    output: {
+        filename: 'compiled',
+        path: `${root}/dist`,
+        libraryTarget: "commonjs"
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+        modules: [
+            `${root}/node_modules`,
+            'node_modules'
+        ]
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: [
+                {
+                    loader: 'ts-loader',
+                }
+            ]
+        }]
+    }
+};
