e4873e49b1cd750c1a9973246629fa243d938816
Charles McCarthy <charles@studybundles.com>
2021-03-27 16:30:30 +0000

    Wait For Instance Public DNS On Launch

--- src/bot-deployments/deploy-bot-ec2.ts
+++ src/bot-deployments/deploy-bot-ec2.ts
@@ -1,4 +1,3 @@
-import { Request, Response } from 'express';
 import { UserData } from './ec2-user-data';
 import * as AWS from 'aws-sdk';
 
@@ -46,9 +45,10 @@ export class DeployBotEc2 {
 						.then(
 							async (tagData) => {
 								console.log('Instance tagged');
-								setInterval(async () => {
-									await DeployBotEc2.GetDeploymentURL(instanceId);
-								}, 5000);
+								DeployBotEc2.GetDeploymentURL(instanceId);
+								// setInterval(async () => {
+								// 	await
+								// }, 5000);
 								resolve({ instanceData, tagData })
 							}
 						).catch(
@@ -65,10 +65,9 @@ export class DeployBotEc2 {
 
 	}
 
-	private static GetDeploymentURL = (instanceId: string) => {
-		console.log('------------ Instance Details ------------');
-		AWS.config.update({ region: 'eu-west-1' });
-		const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+	private static GetDeploymentURL = async (instanceId: string) => {
+		AWS.config.update({region: 'eu-west-1'});
+		const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 		const params = {
 			InstanceIds: [
@@ -76,18 +75,39 @@ export class DeployBotEc2 {
 			]
 		};
 
-		return new Promise((resolve, reject) => {
-			ec2.describeInstances(params, function (err, data) {
-				if (err) {
-					console.log(err);
-				}
+		const interval = setInterval(async () => {
+			const publicDns: string = await new Promise((resolve, reject) => {
+				ec2.describeInstances(params, function (err, data) {
+					if (err) {
+						console.log(err);
+					}
 
-				console.log(data);
-				console.log(data.Reservations?.length && data.Reservations[0].Instances);
-				resolve(data);
+					console.log(data);
+					console.log(data.Reservations?.length && data.Reservations[0].Instances);
+
+					const reservations: AWS.EC2.ReservationList | undefined = data.Reservations;
+
+					if (!reservations || !reservations.length) return resolve(undefined);
+
+					const instances: AWS.EC2.InstanceList | undefined = reservations[0].Instances;
+
+					if (!instances || !instances.length) return resolve(undefined);
+
+					const instance: AWS.EC2.Instance = instances[0];
+					const publicDns: string | undefined = instance.PublicDnsName;
+
+					if (!publicDns) return resolve(undefined);
+
+					resolve(publicDns);
+				});
 			});
-		});
 
+			if (publicDns) {
+				console.log('PUBLIC DNS');
+				console.log(publicDns);
+				clearInterval(interval);
+			}
+		}, 5000);
 	}
 
 
