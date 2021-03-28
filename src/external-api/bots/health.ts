ea53e7f8e36d2ddb7601936d63430e664029d925
Charles McCarthy <charles@studybundles.com>
2021-03-28 00:05:19 +0000

    Bot Health Endpoint Caller

---
+++ src/external-api/bots/health.ts
@@ -0,0 +1,8 @@
+import { HTTP } from '../http';
+
+export class Health {
+
+	public static HealthCheck = async (dns: string): Promise<{ success: boolean }> =>
+		HTTP.get(`http://${dns}:3000/v1/health`)
+
+}
