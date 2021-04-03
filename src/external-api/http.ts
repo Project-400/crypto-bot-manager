1a8ea8599ed000b9a0bd74970962dfe3f55d8c98
Charles McCarthy <charles@studybundles.com>
2021-04-03 20:09:47 +0100

    HTTP Delete Method

--- src/external-api/http.ts
+++ src/external-api/http.ts
@@ -4,7 +4,7 @@ export class HTTP {
 
 	public static async get(url: string): Promise<any> {
 		return new Promise((resolve: any, reject: any): void => {
-			axios.get(`${url}`)
+			axios.get(url)
 				.then((res: AxiosResponse): void => {
 					if (res.status === 200) resolve(res.data);
 					else reject(res);
@@ -18,7 +18,21 @@ export class HTTP {
 
 	public static async post(url: string, postData: any): Promise<any> {
 		return new Promise((resolve: any, reject: any): void => {
-			axios.post(`${url}`, postData)
+			axios.post(url, postData)
+				.then((res: AxiosResponse): void => {
+					if (res.status === 200) resolve(res.data);
+					else reject(res);
+				})
+				.catch((error: AxiosError): void => {
+					// console.error(error);
+					reject(error);
+				});
+		});
+	}
+
+	public static async delete(url: string, postData: any): Promise<any> {
+		return new Promise((resolve: any, reject: any): void => {
+			axios.delete(url, postData)
 				.then((res: AxiosResponse): void => {
 					if (res.status === 200) resolve(res.data);
 					else reject(res);
